f90eccf52a6c0246a4e3a41b82c58cd15878c37c
madvirus-ops <edwinayabie1@gmail.com>
2023-08-01 22:32:13 +0100

    settings

--- src/connections/database.py
+++ src/connections/database.py
@@ -1,3 +1,42 @@
-from sqlalchemy.orm import declarative_base,sessionmaker
+import sys
+sys.path.append("./")
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+
+
 
 Base = declarative_base()
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+
+
+engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, echo = False,pool_size=5)
+#will use alembic for database migrations
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+
+
+def get_db():
+    """ ensures the database connection is always closed 
+        to use this we have to use fastapi.Depends() as an argument in the routes
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
+
+
---
+++ src/settings.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append("./")
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from passlib.context import CryptContext
+
+
+
+class Settings:
+    APP_TITLE = "Edwin Ayabie - Portfolio"
+    ALLOWED_HOST = os.getenv("ALLOWED_HOST").split(",")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = bool(os.getenv("DEBUG"))
+    ALLOWED_PORT = int(os.getenv("PORT"))
+    DB_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+    TEST_DB_URL = os.getenv("SQLALCHEMY_TEST_DATABASE_URL")
+    ACCESS_TOKEN_EXPIRY_TIME = 60 * 30
+    REFRESH_TOKEN_EXPIRY_TIME = 60 * 24 * 365
+    PASSWORD_HASHER = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    JWT_ALGORITHM = "HS256"
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+    PAGE_SIZE = 50
+
+
+settings = Settings()
