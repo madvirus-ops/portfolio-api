d0bea855cafc16922d43f5f07487e89772410780
madvirus-ops <edwinayabie1@gmail.com>
2023-08-03 05:39:41 +0100

    update_cert

--- src/connections/certificate_help.py
+++ src/connections/certificate_help.py
@@ -64,3 +64,24 @@ def get_all_Certificates(db:Session):
         return {"code": 400, "status": "error", "message": e.args}
     
 
+def update_certificate(certificate_id: str,details: CertificateUpdate, db: Session):
+    try:
+        fetch = db.query(Certificate).filter(Certificate.id == certificate_id).first()
+        if fetch is None:
+            return {
+                "code": 404,
+                "status": "error",
+                "message": "Certificate Not Found",
+            }
+        fetch.updated_at = datetime.now(tz)
+        db.commit()
+        hero_data = details.model_dump(exclude_unset=True)
+        for key, value in hero_data.items():
+            setattr(fetch, key, value)
+        db.add(fetch)
+        db.commit()
+        return {"code":200,"status":"success","message":f"{certificate_id} updated"}
+    except Exception as e:
+        print(e.args)
+        return {"code": 400, "status": "error", "message": e.args}
+    
