9b216249d2f95eeb565beaa8b2500517915b90f9
madvirus-ops <edwinayabie1@gmail.com>
2023-08-03 05:38:42 +0100

    create cert hel

---
+++ src/connections/certificate_help.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.path.append("./")
+from connections.models import ProfessionCertificate as Certificate,tz
+from sqlalchemy.orm import Session
+from connections.database import get_db
+from rest_api.rest_schema import CertificateIn,CertificateUpdate
+from datetime import datetime
+
+
+# helper
+def function_name():
+    try:
+        pass
+    except Exception as e:
+        print(e.args)
+        return {"code": 400, "status": "error", "message": e.args}
+
+
+def create_Certificate(details: CertificateIn, db: Session):
+    try:
+        create = Certificate(**details.model_dump())
+        db.add(create)
+        db.commit()
+        db.refresh(create)
+        return {
+                "code": 201,
+                "status": "success",
+                "message": "Certificate created successfully",
+                "id": create.id,
+            }
+
+    except Exception as e:
+        print(e.args)
+        return {"code": 400, "status": "error", "message": e.args}
+
+
